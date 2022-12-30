d2e733c8f6544e8d99c8542d061d701090801333
Andi Fahmi <andifahmi266@gmail.com>
2022-12-30 16:06:25 +0800

    Added Syntax to The Home Screen

--- Src/Screens/Home/Home.js
+++ Src/Screens/Home/Home.js
@@ -1,6 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, Dimensions } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+const { width, heigth } = Dimensions.get('window');
 
 const Home = () => {
     const [data, setData] = useState([]);
@@ -20,8 +23,67 @@ const Home = () => {
     }, [])
 
     return (
-        <View>
-            <Text>Home</Text>
+        <View
+            style={{
+                backgroundColor: '#1e90ff'
+            }}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    paddingVertical: 10,
+                    paddingHorizontal: 10,
+                    backgroundColor: '#000080'
+                }}>
+                <Text
+                    style={{
+                        fontSize: 25,
+                        fontWeight: '600',
+                        color: '#ffffff'
+                    }}>My Quotes</Text>
+                <TouchableOpacity>
+                    <Ionicons
+                        name='search'
+                        size={35}
+                        style={{ color: '#ffffff' }} />
+                </TouchableOpacity>
+            </View>
+            <ScrollView>
+                <View
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        paddingBottom: 100
+                    }}>
+                    {data &&
+                        data.map((item, index) => {
+                            return (
+                                <View key={index}
+                                    style={{
+                                        width: width - 50,
+                                        minHeight: 150,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        marginVertical: 10,
+                                        backgroundColor: '#87ceeb',
+                                    }}>
+                                    <Text
+                                        style={{
+                                            maxWidth: width - 60,
+                                            fontSize:16,
+                                            bottom:20
+                                        }}>{item.quote}</Text>
+                                        <Text
+                                         style={{
+                                            textAlign:'right'
+                                         }}>#{item.author}</Text>
+                                </View>
+                            );
+                        })
+                    }
+                </View>
+            </ScrollView>
         </View>
     )
 }
