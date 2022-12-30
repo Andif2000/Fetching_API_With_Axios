e15070fe761d891bcedd833e080ed9debd531d79
Andi Fahmi <andifahmi266@gmail.com>
2022-12-30 14:04:59 +0800

    Change Syntax of App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+
+import Home from './Src/Screens/Home/Home';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
   );
 }
 
